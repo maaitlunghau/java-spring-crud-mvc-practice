10d01adaaefb01bb8abb68b387f45a864171db83
maaitlunghau <chunhau.py@gmail.com>
2026-05-28 21:52:42 +0700

    chore: initial java spring project from start.spring.io - jsp

---
+++ spring-mvc-jsp/src/main/java/com/practice/spring_mvc_jsp/SpringMvcJspApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spring_mvc_jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcJspApplication.class, args);
+	}
+
+}
---
+++ spring-mvc-jsp/src/test/java/com/practice/spring_mvc_jsp/SpringMvcJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring_mvc_jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
